da325ccb0be8845b7c034dc10b8c94661cbbfaa7
elias <rhyanlucaseliasmartins@gmail.com>
2025-05-14 17:30:01 -0300

    next

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
         hostname: "randomuser.me",
       },
       {
-        protocol: "https",
         hostname: "img.clerk.com",
       },
     ],
   },
-  output: "standalone", // Ou "export" se for estático
 };
 
-module.exports = nextConfig;
+export default nextConfig;
