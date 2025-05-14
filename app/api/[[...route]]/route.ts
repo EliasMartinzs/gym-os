e8a9ffded09c911e933947ebb520b2e570b8f958
elias <rhyanlucaseliasmartins@gmail.com>
2025-05-14 18:50:32 -0300

    route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,11 +3,7 @@ import { handle } from "hono/vercel";
 
 import personal from "./personal";
 
-import type { PageConfig } from "next";
-
-export const config: PageConfig = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
